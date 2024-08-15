1541da61ddc95c4ea4532872018860444d56b067
Ozgur Yuksel <ozgur.yuksel@bisoft.com.tr>
2024-08-15 13:44:25 +0300

    Add centos7 script

---
+++ config-firewall-rh-centos7.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Shell script to enable and configure firewalld on RH 7 based systems
+
+# Enable and start firewalld
+systemctl enable firewalld
+systemctl start firewalld
+
+TCP_PORTS=`netstat -tln | awk '{print $4}' | tail -n +3 | grep -oE '[^:]+$' | sort -u`
+UDP_PORTS=`netstat -uln | awk '{print $4}' | tail -n +3 | grep -oE '[^:]+$' | sort -u`
+
+# Allow TCP ports
+for PORT in $TCP_PORTS
+do
+    firewall-cmd --zone=public --add-port=$PORT/tcp
+done
+
+echo -n "Allowed TCP ports: "
+echo $TCP_PORTS
+
+# Allow UDP ports
+for PORT in $UDP_PORTS
+do
+    firewall-cmd --zone=public --add-port=$PORT/udp
+done
+
+echo -n "Allowed UDP ports: "
+echo $UDP_PORTS
+
+# Make changes permanent
+firewall-cmd --runtime-to-permanent
+
+echo "Made FirewallD rules permanent"
+
+firewall-cmd --list-all
