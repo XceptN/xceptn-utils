2b66b10e3859d7d5312ce9a04a90708d49128918
Ozgur Yuksel <ozgur.yuksel@bisoft.com.tr>
2024-09-21 10:26:55 +0300

    New script

---
+++ list-conn-2-ports-listened-2-RH8-9.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script that lists active connections to network ports 
+# that are listened to. UDP and TCP. Applicable to RH 8/9 compat.
+
+# Get list of TCP ports being listened to
+TCP_PORTS=`ss -tln | awk '{print $4}' | tail -n +2 | grep -oE '[^:]+$' | sort -u`
+
+# Get list of UDP ports being listened to
+UDP_PORTS=`ss -uln | awk '{print $4}' | tail -n +2 | grep -oE '[^:]+$' | sort -u`
+
+# List active connections to listened TCP ports
+for PORT in $TCP_PORTS
+do
+    echo "Connections to TCP port $PORT:"
+    ss -tn | grep ":$PORT " | awk {'print $6'}
+    echo
+done
+
+# List active connections to listened UDP ports
+for PORT in $UDP_PORTS
+do
+    echo "Connections to UDP port $PORT:"
+    ss -un | grep ":$PORT " | awk {'print $6'}
+    echo
+done
