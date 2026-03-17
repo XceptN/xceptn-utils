fbb1de4080c0194007cd4087c7fcad95bc83de3f
Ozgur Yuksel <ozgur.yuksel@bisoft.com.tr>
2026-03-17 19:38:15 +0300

    fix logname

--- backup-etc.sh
+++ backup-etc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Sync /etc into a folder inside your home so Deja Dup picks it up
-DEST="/home/$(logname)/Backup/etc-backup"
+USER_NAME="oyuksel"
+DEST="/home/${USER_NAME}/Backup/etc-backup"
 mkdir -p "$DEST"
 rsync -a --delete /etc/ "$DEST/"
-chown -R $(logname):$(logname) "$DEST"
+chown -R "${USER_NAME}:${USER_NAME}" "$DEST"
